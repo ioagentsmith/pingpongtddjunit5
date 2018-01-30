7ac4e27ef9af64809638d8ae54ba50046f16d376
Christoff Smith <Christoff.Smith@standardbank.co.za>
2018-01-30 10:51:30 +0200

    umar-and-christoffs-changes: our pair programming changes

--- src/main/java/com/bbd/pingpong/RomanConverter.java
+++ src/main/java/com/bbd/pingpong/RomanConverter.java
@@ -4,8 +4,66 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RomanConverter {
-    public static int Convert(String s)
-    {
-        return 1;
+
+    public static int Convert(String s) {
+        if (s == null || s.isEmpty()) {
+            throw new IllegalArgumentException("Invalid Numeral");
+        }
+
+        s = s.toUpperCase();
+        int sum = 0;
+
+
+        for (int i = 0; i < s.length(); i++) {
+
+
+            if (s.charAt(i) == 'I') {
+                sum++;
+            } else if (s.charAt(i) == 'V') {
+                sum += 5;
+            } else if (s.charAt(i) == 'X') {
+                sum = handleNumeral(s, sum, "X", 10);
+            } else if (s.charAt(i) == 'L') {
+                sum = handleNumeral(s, sum, "L", 50);
+            } else if (s.charAt(i) == 'C') {
+                sum = handleNumeral(s, sum, "C", 100);
+            } else if (s.charAt(i) == 'D') {
+                sum = handleNumeral(s, sum, "D", 500);
+            } else if (s.charAt(i) == 'M') {
+                sum = handleNumeral(s, sum, "D", 1000);
+            } else {
+                throw new IllegalArgumentException("Invalid Numeral");
+            }
+
+            //Numerals.valueOf(s.charAt(i));
+        }
+
+        return Math.abs(sum);
     }
+
+    private static int handleNumeral(final String s, int sum, final String numeral, final int value) {
+        if (s.endsWith(numeral)) {
+            sum -= value;
+        } else {
+            sum += value;
+        }
+        return sum;
+    }
+}
+
+enum Numerals {
+    I(1),
+    V(5),
+    X(10),
+    L(50),
+    C(100),
+    D(500),
+    M(1000);
+
+    public int value;
+
+    Numerals(int value) {
+        this.value = value;
+    }
+
 }
--- src/test/java/com/bbd/pingpong/testPing.java
+++ src/test/java/com/bbd/pingpong/testPing.java
@@ -1,16 +1,58 @@
 package com.bbd.pingpong;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.CsvSource;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class testPing {
+
+    //ufkholvadia@gmail.com - Umar
+
+    @ParameterizedTest
+    @CsvSource(
+            {
+            "I, 1",
+            "II, 2",
+            "V, 5",
+            "VI, 6",
+            "vi, 6",
+            "VIII, 8",
+            "IX, 9",
+            "X, 10",
+            "XI, 11",
+            "XIX, 19",
+            "XX, 20",
+            "XXX, 30",
+            "XL, 40",
+            "XC, 90",
+            "C, 100",
+            "D, 500",
+            "M, 1000"
+    })
+    public void testConverter(String numeral, int value) {
+        assertEquals(value, RomanConverter.Convert(numeral));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "a", "b", "null"})
+    public void testException(String numeral) {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            RomanConverter.Convert(numeral);
+        });
+        assertEquals("Invalid Numeral", exception.getMessage());
+    }
+
     @Test
-    public void testNumber1() {
-        assertEquals(1,RomanConverter.Convert("I"));
+    public void testNull() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            RomanConverter.Convert(null);
+        });
+        assertEquals("Invalid Numeral", exception.getMessage());
     }
 
 }
